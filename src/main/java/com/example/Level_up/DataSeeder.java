257d8d145da20335f97ff7c772a1dc5536cfc796
MarioLoyol4 <mar.loyolac@duocuc.cl>
2025-12-03 12:35:40 -0300

    Corregido y modificado para que sea funcional con la App movil. Falta corregir los productos en DataSeeder.

--- src/main/java/com/example/Level_up/DataSeeder.java
+++ src/main/java/com/example/Level_up/DataSeeder.java
@@ -22,35 +22,35 @@ public class DataSeeder {
                 repository.saveAll(Arrays.asList(
                         new Producto(
                                 "CO001",
-                                "PlayStation 5",       // Nombre
-                                "Consolas",            // Categoría
+                                "PlayStation 5",
+                                "Consolas",
                                 "Consola de última generación de Sony.",
                                 549990.0,
                                 "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21"
                         ),
                         new Producto(
                                 "AC001",
-                                "Control Xbox Series X", // Nombre
-                                "Accesorios",            // Categoría
-                                "Mando inalámbrico Robot White.",
+                                "Control Xbox Series X",
+                                "Accesorios",
+                                "Mando inalámbrico Xbox Series X.",
                                 59990.0,
-                                "https://compass-ssl.xbox.com/assets/26/63/2663eb43-42e7-449e-b7df-e9df27d2c332.jpg?n=Xbox-Wireless-Controller_Image-0_Robot-White_1020x574.jpg"
+                                "https://png.pngtree.com/png-clipart/20240901/original/pngtree-wireless-xbox-controller-on-white-png-image_15908916.png"
                         ),
                         new Producto(
                                 "JM001",
-                                "Catan",               // Nombre
-                                "Juegos de Mesa",      // Categoría
+                                "Catan",
+                                "Juegos de Mesa",
                                 "El clásico juego de estrategia.",
                                 34990.0,
-                                "https://images.ctfassets.net/s5n2t79q9icq/4M83Q2yJIoR6t69Y2W96Y/6292437633c36ba4e889b7b7f58d2777/CATAN_3D_box_face.png"
+                                "https://www.pngfind.com/pngs/m/224-2240848_catan-board-game-hd-png-download.png"
                         ),
                         new Producto(
                                 "SG001",
-                                "Secretlab Titan Evo", // Nombre
-                                "Sillas Gamers",       // Categoría
+                                "Secretlab Titan Evo",
+                                "Sillas Gamers",
                                 "Silla ergonómica de nivel profesional.",
                                 389990.0,
-                                "https://secretlab.co/cdn/shop/products/Titan_2020_Black_01.png"
+                                "https://png.pngtree.com/png-vector/20240709/ourlarge/pngtree-black-gaming-chair-png-image_13050960.png"
                         )
                 ));
                 System.out.println("---DATOS DE PRUEBA CARGADOS EN BASE DE DATOS---");
